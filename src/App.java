a7005a3138218c3de10b8ec5fc5291fb69604ffb
hoaian161 <52894214+hoaian161@users.noreply.github.com>
2025-02-18 12:02:44 +0700

    add basic comment checker

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Checker checker = new Checker();
+        while (true) {
+            Checker checker = new Checker();
+        }
     }
 }
--- src/Checker.java
+++ src/Checker.java
@@ -6,17 +6,11 @@ public class Checker {
     Scanner in = new Scanner(System.in);
     private String dir;
     private ArrayList<String> files = new ArrayList<>();
-    private ArrayList<String> raw = new ArrayList<>();
 
     public Checker() {
         this.license();
         if (this.dir()) {
             this.check();
-
-            for (String file : files) {
-                ArrayList<String> raw = raw(file);
-                System.out.println(raw.toString());
-            }
         }
     }
 
@@ -50,7 +44,97 @@ public class Checker {
     }
 
     public void check() {
-        
+        for (String file : files) {
+            boolean isError = false;
+            System.out.println("File " + file + ":");
+            ArrayList<String> raw = raw(file);
+
+            for (int i = 1; i < raw.size() - 1; i++) {
+                String here = raw.get(i).toLowerCase();
+                String before = raw.get(i - 1).toLowerCase();
+                String after = raw.get(i + 1).toLowerCase();
+            
+                if (here.contains("void main") && here.contains("throw")) {
+                    isError = true;
+                    System.out.println("\t- [Line " + (i + 1) + "] Throw error in main method");
+                }
+
+                if (
+                    (
+                        here.contains("public")
+                        || here.contains("private")
+                        || here.contains("static")
+                    )
+                    &&
+                    (
+                        here.contains("(")
+                        && here.contains(")")
+                    )
+                    &&
+                    (
+                        here.contains("{")
+                        || after.contains("{")
+                    )
+                    &&
+                    (
+                        !before.contains("*/")
+                    )
+                ) {
+                    isError = true;
+                    System.out.println("\t- [Line " + (i + 1) + "] Method doesnt have comment");
+                }
+
+                if (
+                    here.contains(" class ")
+                    &&
+                    (
+                        here.contains("{")
+                        || after.contains("{")
+                    )
+                    &&
+                    !before.contains("*/")
+                ) {
+                    isError = true;
+                    System.out.println("\t- [Line " + (i + 1) + "] Class doesnt have comment");
+                }
+
+                if (here.contains("@return")) {
+                    String[] parts = here.split("@return");
+
+                    if (parts.length >= 2) {
+                        if (parts[1].length() < 3) {
+                            isError = true;
+                            System.out.println("\t- [Line " + (i + 1) + "] Return comment might not available");
+                        }
+                    } else {
+                        isError = true;
+                        System.out.println("\t- [Line " + (i + 1) + "] No return comment");
+                    }
+                }
+
+                if (here.contains("@param")) {
+                    String[] paramPath = here.split("@param");
+                    
+                    if (paramPath.length == 1) {
+                        isError = true;
+                        System.out.println("\t- [Line " + (i + 1) + "] Parameter doesnt have enough part");
+                    } else {
+                        String[] paramPaths = paramPath[1].split(" ");
+
+                        if (paramPaths.length <= 2) {
+                            isError = true;
+                            System.out.println("\t- [Line " + (i + 1) + "] Parameter doesnt have comment");
+                        }
+                    }
+                }
+            }
+
+            if (!isError) {
+                System.out.println("\t- SAFE");
+            }
+
+            System.out.println("");
+        }
     }
 
     public ArrayList<String> raw(String item) {
